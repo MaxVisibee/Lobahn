3ac9a59b71e8b147a156abd4caba402a18a3e464
MaxVisibee <visibleonemax@gmail.com>
2021-12-28 18:21:44 +0630

    update lobahn

--- resources/views/frontend/news.blade.php
+++ resources/views/frontend/news.blade.php
@@ -27,8 +27,8 @@
     		<div class="">
             	<div class="relative news-image-container1">
 	                <div class="news-image1 spotlight-img-zoom-out news-border-radius overflow-hidden">
-	                   <!--  <img src="./img/news/1.png" class="w-full object-contain" style="visibility: hidden;" /> -->
-	                    <img src="{{ asset('uploads/new_image/$data->news_image') }}" alt="{{ $data->title ?? '-' }}" class="w-full object-contain" style="visibility: hidden;" />
+	                    <img src="./img/news/1.png" class="w-full object-contain" style="visibility: hidden;" />
+	                    <!-- <img src="{{ asset('uploads/new_image/' . $new->news_image) }}" alt="{{ $new->title ?? '-' }}" class="w-full object-contain" style="visibility: hidden;" /> -->
 	                </div>
 	                <div class="news-content bg-gray news-text-radius">
 	                    <div class="md:flex justify-between px-8">
@@ -45,53 +45,32 @@
 	                </div>
 	            </div>
 	        </div>
-    	@endforeach    
-        <div class="">
-            <div class="relative news-image-container1">
-                <div class="news-image1 spotlight-img-zoom-out news-border-radius overflow-hidden">
-                    <img src="./img/news/1.png" class="w-full object-contain" style="visibility: hidden;" />
-                </div>
-                <div class="news-content bg-gray news-text-radius">
-                    <div class="md:flex justify-between px-8">
-                        <div class="pt-8">
-                            <p class="text-lime-orange text-2xl uppercase font-heavy">Donec sed varius felis</p>
-                            <p class="text-lg text-gray-pale">Category 1</p>
-                        </div>
-                        <p class="pt-8 text-sm text-gray-light1">21 June 2021 15:36</p>
-        
-                    </div>
-                    <div class="text-gray-pale text-lg px-8 py-8 w-94percent">
-                        Proin finibus, leo et dictum vestibulum, lacus nulla tempor neque, vitae scelerisque ex urna sed nibh.
-                        Ut eu nulla sollicitudin, iaculis odio porttitor, congue lacus. Maecenas hendrerit mauris eget volutpat
-                        iaculis. Aenean ...
-                    </div>
-                </div>
-            </div>
-        </div>        
-    </div>
+    	@endforeach
+    </div>   
   
-<div class="pb-8 overflow-auto">
-    <div class="flex md:gap-2">
-        <button id="newcontent-pagination1" type="button" class="newcontent-pagination1 uppercase  focus:outline-none text-gray text-lg font-book newcontent-pagination-btn py-2 md:w-10 px-5 flex justify-center"> 1 </button>
-        <button type="button" onclick="changeNewsPagination(2)" id="pagination2" class=" pagination2
-        uppercase focus:outline-none text-gray text-lg font-book newcontent-pagination-btn py-2
-        md:w-10 px-5 flex justify-center">2</button>
-        <button type="button" id="pagination3" onclick="changeNewsPagination(3)" class="
-        pagination3 uppercase focus:outline-none text-gray text-lg font-book newcontent-pagination-btn py-2 md:w-10 px-5 flex justify-center">3</button>
-        <div class="flex justify-center self-center">
-            <span class="text-lg text-lime-orange ml-2 mr-2">.</span>
-            <span class="text-lg text-lime-orange mr-2">.</span>
-            <span class="text-lg text-lime-orange mr-2">.</span>
-        </div>
-        <button type="button" id="pagination43" onclick="changeNewsPagination(43)" class="
-        pagination43 uppercase focus:outline-none text-gray text-lg font-book
-            newcontent-pagination-btn py-2 md:w-10 px-5 flex justify-center">43</button>
-        <button type="button" id="pagination44" onclick="changeNewsPagination(44)" class="
-        pagination44 uppercase focus:outline-none text-gray text-lg font-book newcontent-pagination-btn
-            py-2 md:w-10 px-5 flex justify-center">&gt;</button>
-        <button type="button" id="pagination45" onclick="changeNewsPagination(45)" class="pagination45
-            uppercase focus:outline-none text-gray text-lg font-book newcontent-pagination-btn        py-2 md:w-10 px-5 flex justify-center">&#62;&#62;</button>
-    </div>
+	<div class="pb-8 overflow-auto">
+	    <div class="flex md:gap-2">
+	        <button id="newcontent-pagination1" type="button" class="newcontent-pagination1 uppercase  focus:outline-none text-gray text-lg font-book newcontent-pagination-btn py-2 md:w-10 px-5 flex justify-center"> 1 </button>
+	        <button type="button" onclick="changeNewsPagination(2)" id="pagination2" class=" pagination2
+	        uppercase focus:outline-none text-gray text-lg font-book newcontent-pagination-btn py-2
+	        md:w-10 px-5 flex justify-center">2</button>
+	        <button type="button" id="pagination3" onclick="changeNewsPagination(3)" class="
+	        pagination3 uppercase focus:outline-none text-gray text-lg font-book newcontent-pagination-btn py-2 md:w-10 px-5 flex justify-center">3</button>
+	        <div class="flex justify-center self-center">
+	            <span class="text-lg text-lime-orange ml-2 mr-2">.</span>
+	            <span class="text-lg text-lime-orange mr-2">.</span>
+	            <span class="text-lg text-lime-orange mr-2">.</span>
+	        </div>
+	        <button type="button" id="pagination43" onclick="changeNewsPagination(43)" class="
+	        pagination43 uppercase focus:outline-none text-gray text-lg font-book
+	            newcontent-pagination-btn py-2 md:w-10 px-5 flex justify-center">43</button>
+	        <button type="button" id="pagination44" onclick="changeNewsPagination(44)" class="
+	        pagination44 uppercase focus:outline-none text-gray text-lg font-book newcontent-pagination-btn
+	            py-2 md:w-10 px-5 flex justify-center">&gt;</button>
+	        <button type="button" id="pagination45" onclick="changeNewsPagination(45)" class="pagination45
+	            uppercase focus:outline-none text-gray text-lg font-book newcontent-pagination-btn py-2 md:w-10 px-5 flex justify-center">&#62;&#62;</button>
+	    </div>
+	</div>
 </div>
 @endsection
 
