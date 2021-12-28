f02beda2b84d4d153d7fb0d4e305b7e8ee451259
ZenoVisibleOne <zeno@visibleone.com>
2021-12-28 17:42:10 +0630

    Fixed CSS

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,74 +1,43 @@
-<div class="w-auto top-0 fixed  md:bg-transparent bg-gray corporate-member-menu">
-    <div class=" md:flex justify-between items-center bg-gray lg:px-14 md:px-9 px-4 py-8">
-        <div>
-            <a class="navbar-brand set-align" href="{{url('/')}}">
-                <img src="{{ asset('/img/lobahn-white.svg') }}" alt="company logo" class="company-logo" />
+<div class="w-auto top-0 fixed  md:bg-transparent bg-gray home-menu">
+    <div class=" md:flex homemenu-bg-div justify-between items-center bg-transparent lg:px-14 md:px-9 px-4 md:py-8 py-4">
+        <div class="md:justify-start justify-center flex ">
+            <a href="{{ route('home') }}">
+                <img src="{{ asset('/img/lobahn-white.svg') }}" alt="company logo" class="companymenu-logo" />
             </a>
         </div>
-        <div class="gap-4 ml-4">
-            <div class="md-custom:flex justify-between mt-1">
+        <div class="gap-4 md:ml-4">
+            <div class="flex justify-between mt-1">
                 <div class="flex justify-around">
                     <p class="justify-center text-21 text-gray-pale whitespace-nowrap hover:text-lime-orange font-book">
                         Lobahn Connect™</p>
-
-
-                    {{-- @guest --}}
-                    @if (!Auth::user() && !Auth::guard('company')->user())
-                        <p
-                            class="md:mb-0 text-21 text-gray-pale whitespace-nowrap md-custom:ml-4 hover:text-lime-orange font-book">
-
-                            <a class="nav-item nav-link" href="{{ route('signup') }}">Sign up</a>
-                            /
-                        </p>
-
-                        <p
-                            class="md:mb-0 text-21 text-gray-pale whitespace-nowrap md-custom:ml-4 hover:text-lime-orange font-book">
-                            <a class="nav-item nav-link" href="{{ route('login') }}">Login </a>
-                        </p>
-
-                    @else
-                        @if (Auth::check())
-                            <p
-                                class="md:mb-0 text-21 text-gray-pale whitespace-nowrap md-custom:ml-4 hover:text-lime-orange font-book">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                                    Candidate Logout
-                                </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                class="d-none">
-                                @csrf
-                            </form>
-                            </p>
-                        @endif
-                        @if (Auth::guard('company')->check())
-                            <p
-                                class="md:mb-0 text-21 text-gray-pale whitespace-nowrap md-custom:ml-4 hover:text-lime-orange font-book">
-                                <a class="dropdown-item" href="{{ route('company.logout') }}" onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                                    Employer Logout
-                                </a>
-                            <form id="logout-form" action="{{ route('company.logout') }}" method="POST"
-                                class="d-none">
-                                @csrf
-                            </form>
-                            </p>
-                        @endif
-                    @endif
-                    {{-- @endguest --}}
+                    <a href="{{ route('signup') }}"
+                        class="home-signup-menu md:mb-0 text-21 text-gray-pale whitespace-nowrap md-custom:ml-4 hover:text-lime-orange font-book">
+                        Sign up</a>
+                    <span
+                        class="home-signup-menu md:mb-0 text-21 text-gray-pale whitespace-nowrap md-custom:ml-4 hover:text-lime-orange font-book">
+                        / </span>
+                    <a href="{{ route('login') }}"
+                        class="home-signup-menu md:mb-0 text-21 text-gray-pale whitespace-nowrap md-custom:ml-4 hover:text-lime-orange font-book">
+                        Login</a>
                 </div>
-                <div class="flex justify-between md-custom:mt-0 mt-4">
+                <div class="flex justify-between">
                     <div id="corporate-search-icon" class="corporate-search-icon flex justify-center ml-4">
-                        <img class="object-contain m-auto" src="{{ asset('./img/search.svg') }}" />
+                        <img class="object-contain m-auto corporate-search-image" src="./img/search.svg" />
                     </div>
-                    <div id="corporate-menu-icon" class="corporate-menu-icon flex ml-6 md:mr-0 mr-8">
-                        <img id="" class="z-10 corporate-menu-img object-contain self-center cursor-pointer m-auto"
-                            src="{{ asset('/img/menu-bar.svg') }}" />
+                    <div id="corporate-menu-icon" class="corporate-menu-icon flex ml-6">
+                        <img id="corporate-menu-img"
+                            class="z-10 corporate-menu-img object-contain self-center cursor-pointer m-auto"
+                            src="./img/menu-bar.svg" />
                         <div class="corporate-menu-content overflow-y-auto absolute hidden -mt-12 right-0">
                             <div class="flex justify-end pt-36 corporate-menu-content-div">
                                 <div class="text-right">
                                     <div class="flex justify-end mr-4 mb-4 mt-4">
                                         <div class="corporate-menu-verticalLine"></div>
                                     </div>
+                                    <a href="#" class="md:hidden">
+                                        <p class="pb-4 text-gray-pale text-21 font-book hover:text-lime-orange">
+                                            Sign up / Login</p>
+                                    </a>
                                     <a href="/membership">
                                         <p class="mb-4 text-gray-pale text-21 font-book hover:text-lime-orange">
                                             Membership</p>
@@ -114,13 +83,12 @@
             <div class="relative">
                 <input id=""
                     class="md:ml-12 ml-4
-            appearance-none bg-gray w-11/12
-                    border-b font-light text-white
-                    text-4xl placeholder-smoke-light1
-                     py-4 leading-tight focus:outline-none"
+                appearance-none bg-gray w-11/12
+                        border-b font-light text-white
+                        md:text-4xl sm:text-2xl text-xl placeholder-smoke-light1
+                         py-4 leading-tight focus:outline-none"
                     type="text" placeholder="Enter a keyword to search..." aria-label="">
-                <img src="{{ asset('/img/search.svg') }}" alt="search image"
-                    class="absolute menu-search-box__image" />
+                <img src="./img/search.svg" alt="search image" class="absolute menu-search-box__image" />
             </div>
             <div class="border border-gray border-t-0 border-l-0 border-r-0 mt-7"></div>
         </div>
