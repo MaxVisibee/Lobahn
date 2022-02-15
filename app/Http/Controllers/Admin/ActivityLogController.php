a25d9f9ec20cfdc3aa3dd9e1d0937a600fb48694
ZenoVisibleOne <zeno@visibleone.com>
2022-02-15 10:23:33 +0630

    activity log

--- app/Http/Controllers/Admin/ActivityLogController.php
+++ app/Http/Controllers/Admin/ActivityLogController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\models\ActivityLog;
+use App\Models\ActivityLog;
 
 class ActivityLogController extends Controller
 {
