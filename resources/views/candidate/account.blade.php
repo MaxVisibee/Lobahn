368c12157d78210a1b84c37f50a0d259f73d06d4
mawinmon <mon@visibleone.com>
2022-02-25 12:06:51 +0000

    update account page layout

--- resources/views/candidate/account.blade.php
+++ resources/views/candidate/account.blade.php
@@ -2,64 +2,66 @@
 @section('content')
     <div class="bg-gray-light2 youraccount-container pt-40 pb-40 mt-24 sm:mt-32 md:mt-0">
         <div class="grid lg:grid-cols-2 grid-cols-1 professional-activities-report-gap-safari gap-2">
-            <div class="bg-white rounded-lg py-8 px-8 ">
-                <div>
-                    <p class="text-2xl text-gray uppercase mb-4 font-heavy">membership status</p>
-                    <div class="overflow-x-auto">
-                        <table class="w-full youraccount-status-table" id="youraccount-status-table">
-                            <thead>
-                                <tr>
-                                    <th class="text-sm text-smoke pr-24 pl-4">Items</th>
-                                    <th class="text-sm text-smoke pr-12">Status</th>
-                                    <th class="text-sm text-smoke pr-12">Expiration</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($active_payments as $active_payment)
+            <div>
+                <div class="bg-white rounded-lg py-8 px-8 ">
+                    <div>
+                        <p class="text-2xl text-gray uppercase mb-4 font-heavy">membership status</p>
+                        <div class="overflow-x-auto">
+                            <table class="w-full youraccount-status-table" id="youraccount-status-table">
+                                <thead>
                                     <tr>
-                                        <td class=" text-base text-gray whitespace-nowrap font-book pr-24 pl-4">
-                                            <p>{{ $active_payment->package->package_title ?? '' }} Subscription</p>
-                                        <td class="text-base text-gray pr-12">
-                                            <div class="py-2">
-                                                @if ($active_payment->status)
+                                        <th class="text-sm text-smoke pr-24 pl-4">Items</th>
+                                        <th class="text-sm text-smoke pr-12">Status</th>
+                                        <th class="text-sm text-smoke pr-12">Expiration</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($active_payments as $active_payment)
+                                        <tr>
+                                            <td class=" text-base text-gray whitespace-nowrap font-book pr-24 pl-4">
+                                                <p>{{ $active_payment->package->package_title ?? '' }} Subscription</p>
+                                            <td class="text-base text-gray pr-12">
+                                                <div class="py-2">
+                                                    @if ($active_payment->status)
+                                                        <p
+                                                            class="text-gray text-sm px-2 rounded-lg inline-block bg-lime-orange text-center">
+                                                            Active
+                                                        </p>
+                                                    @else
+                                                        <p
+                                                            class="text-white text-sm px-2 rounded-lg inline-block bg-coral-dark text-center">
+                                                            Expired</p>
+                                                    @endif
+
+                                                </div>
+                                            </td>
+                                            <td class="text-base text-smoke whitespace-nowrap pr-12">
+                                                {{ $active_payment->package_end_date }}</td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td class=" text-base text-gray whitespace-nowrap font-book pr-24 pl-4">
+                                                <p>Free Trial</p>
+                                            <td class="text-base text-gray pr-12">
+                                                <div class="py-2">
+
                                                     <p
                                                         class="text-gray text-sm px-2 rounded-lg inline-block bg-lime-orange text-center">
                                                         Active
                                                     </p>
-                                                @else
-                                                    <p
-                                                        class="text-white text-sm px-2 rounded-lg inline-block bg-coral-dark text-center">
-                                                        Expired</p>
-                                                @endif
 
-                                            </div>
-                                        </td>
-                                        <td class="text-base text-smoke whitespace-nowrap pr-12">
-                                            {{ $active_payment->package_end_date }}</td>
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td class=" text-base text-gray whitespace-nowrap font-book pr-24 pl-4">
-                                            <p>Free Trial</p>
-                                        <td class="text-base text-gray pr-12">
-                                            <div class="py-2">
+                                                </div>
+                                            </td>
+                                            <td class="text-base text-smoke whitespace-nowrap pr-12">
+                                                {{ date('d M Y', strtotime(Auth::user()->package_end_date)) }}
+                                                {{-- {{ date_diff(new \DateTime(Auth::user()->package_start_date),new \DateTime(Auth::user()->package_end_date))->format('%m Months, %d days') }} --}}
+                                            </td>
+                                        </tr>
+                                    @endforelse
 
-                                                <p
-                                                    class="text-gray text-sm px-2 rounded-lg inline-block bg-lime-orange text-center">
-                                                    Active
-                                                </p>
-
-                                            </div>
-                                        </td>
-                                        <td class="text-base text-smoke whitespace-nowrap pr-12">
-                                            {{ date('d M Y', strtotime(Auth::user()->package_end_date)) }}
-                                            {{-- {{ date_diff(new \DateTime(Auth::user()->package_start_date),new \DateTime(Auth::user()->package_end_date))->format('%m Months, %d days') }} --}}
-                                        </td>
-                                    </tr>
-                                @endforelse
-
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
