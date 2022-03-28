5d29ee6d7732d63a2713ab50b023214173369832
ZenoVisibleOne <zeno@visibleone.com>
2022-03-28 08:52:09 +0630

    update

--- resources/views/frontend/news-detail.blade.php
+++ resources/views/frontend/news-detail.blade.php
@@ -178,7 +178,7 @@
             //$('.p1 , ul li , p').removeAttr('style');
             $('.p1 , ul li , p , strong').addClass(
                 'newsdetail-letterspacing w-full text-21 text-gray-pale font-book pt-4 pb-4');
-            $('a').addClass('text-gray-pale a');
+            $('a').addClass('text-lime-orange');
             $('div.content').children('p').addClass(
                 'newsdetail-letterspacing w-full text-21 text-gray-pale font-book pt-4 pb-4');
 
