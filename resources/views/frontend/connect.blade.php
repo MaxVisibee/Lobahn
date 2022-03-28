0c91b2b2ec291b5a3d9ce316e9bd4618fae33b9b
ZenoVisibleOne <zeno@visibleone.com>
2022-03-28 09:21:15 +0630

    update

--- resources/views/frontend/connect.blade.php
+++ resources/views/frontend/connect.blade.php
@@ -1,111 +1,109 @@
 @extends('layouts.frontend-master')
 
-@section('content')
-    <div class="relative lg:mt-0 md:mt-24 mt-20">
-        <img src="./img/premium/1.png" class="w-full object-cover events-banner-container-img" />
-        <div class="absolute premium-content top-1/2 left-1/2">
-            <p class="text-white lg:text-5xl text-4xl font-book whitespace-nowrap text-center">
-                {{ $connect->title ?? '' }}<sup class="font-book md:text-lg text-base">TM</sup></p>
-        </div>
-    </div>
+<div class="md:hidden lobahn-connect-header pb-3">
+    <p class="justify-center text-center text-21 text-gray-pale whitespace-nowrap hover:text-lime-orange font-book">
+        <a href="#" class="cursor-pointer">Lobahn Connect™ </a>
+    </p>
+</div>
 
-    <div class="xl:flex w-full bg-gray-warm-pale">
-        <div class="bg-gray xl:w-6/12 h-auto w-full relative flex justify-center py-24">
-            <div class="w-full lobahn-desc-content flex justify-center text-center self-center">
-                <div class="description">
+
+</div>
+<div class="relative lg:mt-0 md:mt-24 mt-20">
+    <img src="./img/premium/1.png" class="w-full object-cover events-banner-container-img" />
+    <div class="absolute premium-content top-1/2 left-1/2">
+        <p class="text-white lg:text-5xl text-4xl font-book whitespace-nowrap text-center">Lobahn Connect<sup
+                class="font-book md:text-lg text-base">TM</sup></p>
+    </div>
+</div>
+<div class="xl:flex w-full bg-gray-warm-pale">
+    <div class="bg-gray xl:w-6/12 h-auto w-full relative flex justify-center py-24">
+        <div class="w-full lobahn-desc-content flex justify-center text-center self-center">
+            <div class="">
+                {{-- <p
+                    class="text-center xl:text-5xl md:text-4xl text-3xl text-lime-orange 2xl-custom-1440:mb-4 mb-2 uppercase font-book">
+                    Lobahn Connect<sup class="text-lg">TM</sup></p> --}}
+                <div class="description flex justify-center">
+                    {{-- <p
+                        class="text-center text-gray-pale text-21 font-book leading-snug font-futura-pt lobahn-desc-text">
+
+                    </p> --}}
                     {!! $connect->description_one ?? '' !!}
                 </div>
             </div>
         </div>
-        <div class=" xl:w-6/12 w-full">
-            <img class="lobahn-content-img w-full" src="{{ asset('uploads/connect/' . $connect->image_one ?? '') }}" />
-        </div>
     </div>
-    <div class="xl:flex flex-row-reverse w-full">
-        <div class="bg-gray xl:w-6/12 lg:h-auto h-auto w-full relative flex justify-center py-20">
-            <div class="w-full text-center self-center">
-                <div class="flex justify-center w-full">
-                    {{-- <p class="text-center lobahn-content2-desc text-gray-pale text-21 mb-3 leading-snug font-book">
-                    Lobahn Connect™ is a Members-only network of high-potential professionals and leading employers.
-                    Strict
-                    criteria are applied to the talents and enterprises accepted into the Lobahn™ network. We get to
-                    know
-                    well each professional's background, skills and ambitions. Likewise, we carefully vet the employers
-                    who
-                    join the network and the career opportunities lodged with us.
+    <div class=" xl:w-6/12 w-full">
+        <img class="lobahn-content-img w-full" src="./img/howWeWork/1.png" />
+    </div>
+</div>
+<div class="xl:flex flex-row-reverse w-full">
+    <div class="bg-gray xl:w-6/12 lg:h-auto h-auto w-full relative flex justify-center py-20">
+        <div class="w-full text-center self-center">
+            <div class="description flex justify-center w-full">
+                {{-- <p class="text-center lobahn-content2-desc text-gray-pale text-21 mb-3 leading-snug font-book">
+
                 </p> --}}
-                    {!! $connect->description_two ?? '' !!}
-                </div>
+                {!! $connect->description_two ?? '' !!}
             </div>
         </div>
-        <div class=" xl:w-6/12 w-full">
-            <img class="w-full lobahn-content2-img" src="{{ asset('uploads/connect/' . $connect->image_two ?? '') }}" />
-        </div>
     </div>
-    <div class="xl:flex w-full">
-        <div class="bg-gray xl:w-6/12 lg:h-auto h-auto w-full relative flex justify-center py-20">
-            <div class="w-full text-center self-center">
-                <div class="flex justify-center">
-                    {{-- <p class="text-center lobahn-content3-desc text-gray-pale text-21 mb-3 leading-snug font-book">
-                    Members
-                    are invited to attend curated monthly events tailored to their interests, from small-group private
-                    events to network-wide gatherings, where Members learn from each other, discover new opportunities
-                    together and celebrate each other's success.</p> --}}
-                    {!! $connect->description_three ?? '' !!}
-                </div>
-            </div>
-        </div>
-        <div class=" xl:w-6/12 w-full">
-            <img class="lobahn-content3-img w-full" src="{{ asset('uploads/connect/' . $connect->image_three ?? '') }}" />
-        </div>
+    <div class=" xl:w-6/12 w-full">
+        <img class="w-full lobahn-content2-img" src="./img/howWeWork/2.png" />
     </div>
-    <div class="xl:flex flex-row-reverse w-full">
-        <div class="bg-gray xl:w-6/12 lg:h-auto h-auto w-full relative flex justify-center py-20">
-            <div class="w-full text-center self-center">
-                <div class="flex justify-center">
-                    {{-- <p class="text-center text-gray-pale text-21 mb-3 lobahn-content4-desc leading-snug font-book">
-                    Meanwhile, Lobahn's™ advanced algorithms are spinning away to bring Members and career opportunities
-                    together smoothly and efficiently. Individual Members are invited to connect directly with Corporate
-                    Members to discuss the future together. It's clean, efficient and professional.
+</div>
+<div class="xl:flex w-full">
+    <div class="bg-gray xl:w-6/12 lg:h-auto h-auto w-full relative flex justify-center py-20">
+        <div class="w-full text-center self-center">
+            <div class="description flex justify-center">
+                {{-- <p class="text-center lobahn-content3-desc text-gray-pale text-21 mb-3 leading-snug font-book">Members
                 </p> --}}
-                    {!! $connect->description_four ?? '' !!}
-                </div>
-
+                {!! $connect->description_three ?? '' !!}
             </div>
-        </div>
-        <div class=" xl:w-6/12 w-full">
-            <img class="lobahn-content4-img w-full" src="{{ asset('uploads/connect/' . $connect->image_four ?? '') }}" />
         </div>
     </div>
-    @if (!Auth::user() && !Auth::guard('company')->user())
-        <div class="guarantee-container flex justify-center w-full relative bg-lime-orange md:py-40 py-12">
-            <div class="guarantee-contentd">
-                <div>
-                    <p
-                        class="text-center uppercase font-futura-pt xl:text-5xl md:text-4xl text-3xl md:whitespace-nowrap text-gray font-book">
-                        THE LOBAHN™ GUARANTEES</p>
-                    <p class="text-center text-21 text-gray pt-7 font-book">
-                        If you are not completely satisfied with our fixed-fee Talent Discovery™ service, we will refund
-                        your
-                        payment in full upon request. If a Member Professional hired on the 17% success basis leaves your
-                        employment within six months, we will introduce you to new talents at no additional cost until a
-                        suitable replacement is found.
-                    </p>
-                    <div class="flex justify-center pt-8">
+    <div class=" xl:w-6/12 w-full">
+        <img class="lobahn-content3-img w-full" src="./img/howWeWork/3.png" />
+    </div>
+</div>
+<div class="xl:flex flex-row-reverse w-full">
+    <div class="bg-gray xl:w-6/12 lg:h-auto h-auto w-full relative flex justify-center py-20">
+        <div class="w-full text-center self-center">
+            <div class="description flex justify-center">
+                {{-- <p class="text-center text-gray-pale text-21 mb-3 lobahn-content4-desc leading-snug font-book">
+                </p> --}}
+                {!! $connect->description_four ?? '' !!}
+            </div>
 
-                        <a href="{{ url('/signup') }}">
-
-
-                            <button type="button"
-                                class=" whitespace-nowrap text-lg focus:outline-none text-center  text-gray font-futura-pt font-heavy guarantee-join-btn py-4 md:w-96 md:px-28 px-20">Join
-                                Today</button>
-                        </a>
-                    </div>
-                </div>
-
+        </div>
+    </div>
+    <div class=" xl:w-6/12 w-full">
+        <img class="lobahn-content4-img w-full" src="./img/howWeWork/4.png" />
+    </div>
+</div>
+<div class="guarantee-container flex justify-center w-full relative bg-lime-orange md:py-40 py-36">
+    <div class="guarantee-contentd">
+        <div>
+            <p
+                class="text-center uppercase font-futura-pt xl:text-5xl lg:text-4xl text-3xl md:whitespace-nowrap text-gray font-book">
+                THE LOBAHN™
+                GUARANTEES</p>
+            <p class="text-center text-21 text-gray pt-7 font-book">
+                If you are not completely satisfied with any aspect of our Lobahn Connect™ service, we will refund the
+                unused portion of your membership fee upon request.
+            </p>
+            <div class="flex justify-center pt-8">
+                <a href="{{ url('/signup') }}">
+                    <button type="button"
+                        class=" whitespace-nowrap text-lg focus:outline-none text-center  text-gray font-futura-pt font-heavy guarantee-join-btn py-4 md:w-96 md:px-28 px-20">Join
+                        Today</button>
+                </a>
             </div>
         </div>
-    @endif
+
+    </div>
+</div>
+
+@section('content')
 @endsection
 
 @push('scripts')
@@ -118,7 +116,7 @@
 
     <script>
         $(document).ready(function() {
-            $('.description flex p').removeAttr('style');
+            $('.description *').removeAttr('style');
             $('.description flex p').addClass(
                 'text-center text-gray-pale text-21 font-book leading-snug font-futura-pt lobahn-desc-text');
         });
