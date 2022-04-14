7f9ce4759d6bf6ee08ded3a83249cc7ec02fcf10
ZenoVisibleOne <zeno@visibleone.com>
2022-04-14 08:08:19 +0630

    update

---
+++ resources/views/admin/mail/custom.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.layouts.master')
+
+
+@section('content')
+    <!-- begin #content -->
+    <!-- <div id="content" class="content"> -->
+    <!-- begin breadcrumb -->
+    <ol class="breadcrumb float-xl-right">
+        <li class="breadcrumb-item"><a href="javascript:;">Home</a></li>
+        <li class="breadcrumb-item active"><a href="javascript:;">Coupon</a></li>
+    </ol>
+    <!-- end breadcrumb -->
+
+    <!-- begin page-header -->
+    <h4 class="bold content-header">Mail Management<small> </small></h4>
+    <div id="footer" class="footer" style="margin-left: 0px"></div>
+    <div class="row m-b-10">
+        <div class="col-lg-12">
+            <div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <!-- begin row -->
+    <div class="row">
+        <!-- begin col-6 -->
+        <div class="col-xl-12">
+            <!-- begin panel -->
+            <div class="panel panel-inverse" data-sortable-id="form-stuff-1">
+                <!-- begin panel-heading -->
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        Create New Coupon
+                    </h4>
+                    <div class="panel-heading-btn">
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default"
+                            data-click="panel-expand"><i class="fa fa-expand"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success"
+                            data-click="panel-reload"><i class="fa fa-redo"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning"
+                            data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+                        <!-- <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a> -->
+                    </div>
+                </div>
+                <!-- end panel-heading -->
+                <!-- begin panel-body -->
+                <div class="panel-body">
+
+                </div>
+                <!-- end panel-body -->
+            </div>
+        </div>
+    @endsection
