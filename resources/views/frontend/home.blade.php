ae55480df33aac366fcc9f99ee64b1bc1ed23c93
ZenoVisibleOne <zeno@visibleone.com>
2022-04-29 14:20:08 +0630

    Feature Opportunity Update

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -178,8 +178,8 @@
                                             </div>
                                             <p data-value="@foreach ($seeker->jobPositions as $value) {{ $value->job_title ?? '-' }}
                                                 @if (!$loop->last), @endif @endforeach
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    -
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    {{ $seeker->carrier->carrier_level ?? '' }}"
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            -
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            {{ $seeker->carrier->carrier_level ?? '' }}"
                                                 class="md:text-21 text-lg font-heavy text-gray-pale pb-8 slider-position-title{{ $key }} position-title-text">
                                                 @if (isset($seeker->carrier->carrier_level))
                                                     - {{ $seeker->carrier->carrier_level }}
@@ -457,144 +457,7 @@
                             @endforeach
                         </div>
                     </div>
-
                 </div>
-                {{-- <div class="flex xl:w-3/5 md:w-90percent m-auto w-full justify-center">
-                    <div class="w-90percent">
-                        <div class="feature-opportunity-carousel">
-                            @foreach ($opportunities as $key => $opportunity)
-                                <div class="flex 3xl-custom:px-0 px-4">
-                                    <div class="lg:flex justify-center">
-                                        <div class="relative">
-                                            <div class="bg-gray-light rounded-corner relative opportunity-container">
-                                                <div class="md:p-12 p-4">
-                                                    <div
-                                                        class="mobile-slick-icon absolute top-2/4 -left-3 justify-center self-center feature-opportunity-previous cursor-pointer">
-                                                        <img
-                                                            src="{{ asset('/img/home/feature/Icon feather-arrow-left.png') }}" />
-                                                    </div>
-                                                    <div class="flex justify-between">
-                                                        <div>
-                                                            <h1 class="text-xl md:text-2xl lg:text-3xl xl:text-4xl text-lime-orange mt-4 mb-2 featured-opportunity-title{{ $key }}"
-                                                                data-value="{{ $opportunity->title }}">
-                                                                {{ $opportunity->title }}</h1>
-                                                            <div
-                                                                class="text-base lg:text-lg text-gray-light1 letter-spacing-custom">
-                                                                <span class="featured-opportunity-name{{ $key }}"
-                                                                    data-value="{{ $opportunity->company->company_name }}">{{ $opportunity->company->company_name }}</span>
-                                                            </div>
-                                                        </div>
-                                                        <div class="flex justify-end opportunity-logo">
-                                                            <img alt="company logo" class="m-auto"
-                                                                src="{{ $opportunity->company->company_logo? asset('/uploads/company_logo/' . $opportunity->company->company_logo): asset('images/default.png') }}">
-                                                        </div>
-                                                    </div>
-                                                    <div class="flex flex-wrap justify-between pr-4 w-4/5 mt-4">
-                                                        <div class="flex pr-4">
-                                                            <img src="{{ asset('/img/industry.svg') }}"
-                                                                class="w-auto" />
-                                                            @php
-                                                                $industries = DB::table('industry_usages')
-                                                                    ->where('opportunity_id', $opportunity->id)
-                                                                    ->get()
-                                                                    ->pluck('industry_id');
-                                                            @endphp
-                                                            <p class="font-futura-pt text-lg text-gray-pale pl-2">
-                                                                @foreach ($industries as $industry_id)
-                                                                    {{ DB::table('industries')->where('id', $industry_id)->get()->pluck('industry_name')[0] }}
-                                                                    @if (!$loop->last)
-                                                                        ,
-                                                                    @endif
-                                                                @endforeach
-                                                            </p>
-                                                        </div>
-                                                        @isset($opportunity->carrier->carrier_level)
-                                                            <div class="flex pr-4">
-                                                                <img src="{{ asset('/img/people.svg') }}"
-                                                                    class="w-auto" />
-                                                                <p class="font-futura-pt text-lg text-gray-pale pl-2">
-                                                                    {{ $opportunity->carrier->carrier_level ?? '' }}
-                                                                </p>
-                                                            </div>
-                                                        @endisset
-                                                        <div class="flex">
-                                                            <img src="{{ asset('/img/area.svg') }}"
-                                                                class="w-auto" />
-                                                            @php
-                                                                $areas = DB::table('functional_area_usages')
-                                                                    ->where('opportunity_id', $opportunity->id)
-                                                                    ->get()
-                                                                    ->pluck('functional_area_id');
-                                                            @endphp
-                                                            <p class="font-futura-pt text-lg text-gray-pale pl-2">
-                                                                @foreach ($industries as $area_id)
-                                                                    {{ DB::table('functional_areas')->where('id', $area_id)->get()->pluck('area_name')[0] }}
-                                                                    @if (!$loop->last)
-                                                                        ,
-                                                                    @endif
-                                                                @endforeach
-                                                            </p>
-                                                        </div>
-                                                    </div>
-                                                    <div
-                                                        class="border border-gray-pale border-t-0 border-l-0 border-r-0 my-4">
-                                                    </div>
-                                                    <ul
-                                                        class="mt-6 mb-10 text-white mark-yellow xl:text-2xl sm:text-xl text-lg">
-                                                        @isset($opportunity->highlight_1)
-                                                            <li class="mb-4">{{ $opportunity->highlight_1 }}</li>
-                                                        @endisset
-                                                        @isset($opportunity->highlight_2)
-                                                            <li class="mb-4">{{ $opportunity->highlight_2 }}</li>
-                                                        @endisset
-                                                        @isset($opportunity->highlight_3)
-                                                            <li class="mb-4">{{ $opportunity->highlight_3 }}</li>
-                                                        @endisset
-                                                    </ul>
-                                                    @if (isset($opportunity->highlight_1) || isset($opportunity->highlight_2) || isset($opportunity->highlight_3))
-                                                        <div
-                                                            class="border border-gray-pale border-t-0 border-l-0 border-r-0 my-4">
-                                                        </div>
-                                                    @endif
-                                                    <div class="mt-7 opportunity-description">
-                                                        {!! $opportunity->description !!}
-                                                    </div>
-                                                    <div class="tag-bar mt-7 text-sm">
-                                                        @php
-                                                            $keywords = DB::table('keyword_usages')
-                                                                ->where('opportunity_id', $opportunity->id)
-                                                                ->get()
-                                                                ->pluck('keyword_id');
-                                                        @endphp
-                                                        @foreach ($keywords as $keyword_id)
-                                                            <span
-                                                                class="bg-gray-light1 border border-gray-light1 text-tag-color rounded-full px-3 pb-0.5 inline-block mb-2">
-                                                                {{ DB::table('keywords')->where('id', $keyword_id)->get()->pluck('keyword_name')[0] }}
-                                                            </span>
-                                                        @endforeach
-                                                    </div>
-                                                    <div class="button-bar sm:mt-5">
-                                                        <a @if (!Auth::user() && !Auth::guard('company')->user()) href="{{ route('login') }}"
-                                                        @else
-                                                            @if (Auth::user())  href="{{ route('candidate.dashboard') }}" @else  href="{{ route('company.home') }}" @endif
-                                                            @endif
-                                                            class="focus:outline-none text-gray bg-lime-orange text-sm sm:text-base xl:text-lg hover:text-lime-orange hover:bg-transparent border border-lime-orange custom-rounded-btn py-3 px-7 mr-4 full-detail-btn inline-block">View
-                                                            Full Details</a>
-                                                    </div>
-                                                    <div
-                                                        class="mobile-slick-icon absolute top-2/4 -right-3 justify-center self-center feature-opportunity-next cursor-pointer">
-                                                        <img
-                                                            src="{{ asset('/img/home/feature/Icon feather-arrow-right.png') }}" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
-                    </div>
-                </div> --}}
                 <div class="lg:flex hidden justify-center w-10percent self-center">
                     <div class="flex justify-center self-center feature-opportunity-next cursor-pointer">
                         <img src="{{ asset('/img/home/feature/Icon feather-arrow-right.png') }}" />
