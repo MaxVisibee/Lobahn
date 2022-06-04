152d994eb4834a35e5e4c84b9a1065f074c98528
ZenoVisibleOne <zeno@visibleone.com>
2022-06-04 17:11:50 +0630

    update

--- resources/views/candidate/profile-edit.blade.php
+++ resources/views/candidate/profile-edit.blade.php
@@ -105,6 +105,23 @@
       </div>
   </div>
 
+  <div class="fixed top-0 w-full h-screen hidden left-0 z-50 bg-black-opacity" id="delete-cv">
+      <div class="text-center text-white absolute top-1/2 left-1/2 popup-text-box bg-gray-light">
+          <div
+              class="flex flex-col justify-center items-center popup-text-box__container popup-text-box__container-corporate popup-text-box__container--height pt-10 pb-12 relative">
+              <h1 class="text-lg lg:text-2xl tracking-wide popup-text-box__title mb-4">REMOVE CV</h1>
+              <p class="text-gray-pale popup-text-box__description connect-employer-text-box">Are you sure to remove CV from your profile.</p>
+              <div class="button-bar button-bar--width mt-4">
+                <a 
+                    class="del-cv btn-bar focus:outline-none text-gray bg-lime-orange text-sm lg:text-lg hover:text-lime-orange hover:bg-transparent border border-lime-orange rounded-corner py-2 px-4 mr-2">Yes, please</a>
+                <a onclick="toggleModalClose('#delete-cv')"
+                    class="btn-bar focus:outline-none text-gray-pale bg-smoke-dark text-sm lg:text-lg hover:bg-transparent border border-smoke-dark rounded-corner py-2 px-4">Not now 
+                </a>
+              </div>
+          </div>
+      </div>
+  </div>
+
     <!-- success popup -->
     <div class="fixed top-0 w-full h-screen left-0 hidden z-50 bg-black-opacity" id="success-popup">
         <div class="text-center text-gray-pale absolute top-1/2 left-1/2 popup-text-box bg-gray-light">
@@ -141,7 +158,7 @@
                     <img src="{{ asset('/img/sign-up/close.svg') }}" alt="close modal image">
                 </button>
                 <p class="text-base lg:text-lg tracking-wide popup-text-box__title mb-4 letter-spacing-custom">
-                    {{ session('error') ?? 'Something went wrong , please try again!' }}</p>
+                    {{ session('error') ?? 'Wrong password, please try again!' }}</p>
             </div>
         </div>
     </div>
@@ -1061,9 +1078,9 @@
                                                 <img src="{{ asset('/img/member-profile/Icon awesome-eye.svg') }}"
                                                     alt="eye icon" class="h-2.5" />
                                             </button></a>
-                                        <button type="button" class="focus:outline-none delete-cv-button">
+                                        <button type="button" class="focus:outline-none">
                                             <img src="{{ asset('/img/member-profile/Icon material-delete.svg') }}"
-                                                alt="delete icon" class="del-cv" style="height:0.884rem;" />
+                                                alt="delete icon" style="height:0.884rem;" class="to-delete-cv"/>
                                         </button>
                                         <div class="bg-lime-orange py-0 cv-tooltip">
                                             <span class="text-gray text-sm">Set as default</span>
@@ -2943,22 +2960,21 @@
                 $('#go-back').removeClass('hidden')
                 $('#go-back').css('display','block')
             })
-           
 
 
-            @if (session('success'))
-                @php
-                    Session::forget('success');
-                @endphp
-                openModalBox('#success-popup');
-                openMemberProfessionalProfileEditPopup();
-            @endif
-            @if (session('error'))
-                @php
-                    Session::forget('error');
-                @endphp
-                openModalBox('#error-popup');
-            @endif
+            // @if (session('success'))
+            //     @php
+            //         Session::forget('success');
+            //     @endphp
+            //     openModalBox('#success-popup');
+            //     openMemberProfessionalProfileEditPopup();
+            // @endif
+            // @if (session('error'))
+            //     @php
+            //         Session::forget('error');
+            //     @endphp
+            //     openModalBox('#error-popup');
+            // @endif
 
             // Left Side 
 
@@ -3470,6 +3486,8 @@
                             $('#change-password-form').next().removeClass('hidden')
                         } else {
                             $('#error-popup').removeClass('hidden')
+                            $('#error-popup').css('display', 'block')
+                            $('#loader').addClass('hidden')
                         }
                     },
                     beforeSend: function() {
@@ -3605,9 +3623,9 @@
                                                 <img src="{{ asset('/img/member-profile/Icon awesome-eye.svg') }}" alt="eye icon"
                                                     class="h-2.5" />
                                             </button>
-                                            <button type="button" class="focus:outline-none delete-cv-button">
+                                            <button type="button" class="focus:outline-none">
                                                 <img src="{{ asset('./img/member-profile/Icon material-delete.svg')}}"
-                                                    alt="delete icon" class="del-cv" style="height:0.884rem;" />
+                                                    alt="delete icon" class="to-delete-cv" style="height:0.884rem;" />
                                             </button>
                                             <div class="bg-lime-orange py-0 cv-tooltip">
                                                 <span class="text-gray text-sm">Set as default</span>
@@ -3650,16 +3668,25 @@
             });
 
 
-            $('.del-cv').click(function(e) {
+            var cv; 
+            $(document).on("click", ".to-delete-cv" , function() {
+                $('#delete-cv').removeClass('hidden')
+                $('#delete-cv').css('display','block')
+                cv = $(this)
+            })
+
+            $(document).on("click", ".del-cv" , function(e) {
+                $('#delete-cv').addClass('hidden')
+                $('#delete-cv').css('display','none')
                 $("#loader").removeClass('hidden')
                 e.preventDefault();
-                removeCVFile('#cv-'+$(this).parent().next().next().val())
+                removeCVFile('#cv-'+cv.parent().next().next().val())
                 $.ajax({
                     type: 'POST',
                     url: 'cv-delete',
                     data: {
                         "_token": "{{ csrf_token() }}",
-                        'id': $(this).parent().next().next().val()
+                        'id': cv.parent().next().next().val()
                     },
                     success: function(data) {
                         $("#delete-popup").removeClass('hidden')
